d2f5be896f24f59d04d4c6b56320447fa5c7ad30
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-15 01:48:46 +0200

    Removing HeaderWrapper

--- compounds/HeaderCompound.js
+++ compounds/HeaderCompound.js
@@ -1,12 +1,9 @@
-import HeaderWrapper from "../components/Header/HeaderWrapper"
 import Navbar from "../components/Header/Navbar"
 
 export default function HeaderCompound() {
   return (
     <>
-      <HeaderWrapper>
         <Navbar />
-      </HeaderWrapper>
     </>
   )
 }
