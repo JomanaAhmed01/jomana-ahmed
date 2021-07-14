582100cb07720ce4e398e54269eb9552c6d43bfc
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-15 01:33:42 +0200

    Adding the Background-image

--- components/Header/HeaderWrapper.js
+++ components/Header/HeaderWrapper.js
@@ -1,3 +1,9 @@
+import styled from "styled-components"
+
 export default function HeaderWrapper() {
-  return <></>
+  return <Wrapper></Wrapper>
 }
+
+export const Wrapper = styled.div`
+  background-image: url('/images/header-background.jpg');
+`
