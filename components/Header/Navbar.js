0b060a20b39bbe0feb45ce8fb6791eaacbd2e398
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-15 01:47:47 +0200

    Adding style-component to Navbar

--- components/Header/Navbar.js
+++ components/Header/Navbar.js
@@ -1,3 +1,9 @@
+import styled from "styled-components"
+
 export default function Navbar() {
-  return <></>
+  return <Wrapper>hghhhhhhhhhhhh</Wrapper>
 }
+
+export const Wrapper = styled.div`
+  border: 3px solid red;
+`
