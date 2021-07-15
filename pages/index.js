89c06ff4b1777afd99b0d98c258815929a04e6af
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-16 00:00:35 +0200

    Creating the AboutCompound file

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import HeaderCompound from '../compounds/HeaderCompound'
+import AboutCompound from "../compounds/AboutCompound"
 
 export default function Home() {
   return (
     <>
       <HeaderCompound />
+      <AboutCompound />
     </>
   )
 }
