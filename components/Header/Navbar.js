f39d97fe0b8c1d6faf592316c5b204978c219889
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-15 02:31:41 +0200

    Creating the Navbar

--- components/Header/Navbar.js
+++ components/Header/Navbar.js
@@ -1,9 +1,45 @@
 import styled from "styled-components"
 
 export default function Navbar() {
-  return <Wrapper>hghhhhhhhhhhhh</Wrapper>
+  return (
+    <Wrapper>
+      <LinkSpecial>Home</LinkSpecial>
+      <Link>About</Link>
+      <Link>Works</Link>
+      <Link>Contact</Link>
+    </Wrapper>
+  )
 }
 
 export const Wrapper = styled.div`
-  border: 3px solid red;
+  display: flex;
+  justify-content: center;
+`
+
+export const LinkSpecial = styled.p`
+  padding: 8px 13px;
+  line-height: 32px;
+  text-decoration: none;
+  color: #f06000;
+  font-weight: bold;
+  cursor: pointer;
+
+  :hover {
+    color: #ffffff;
+    transition: 0.5s;
+  }
+`
+
+export const Link = styled.p`
+  padding: 8px 13px;
+  line-height: 32px;
+  text-decoration: none;
+  color: #ffffff;
+  font-weight: bold;
+  cursor: pointer;
+
+  :hover {
+    color: #f06000;
+    transition: 0.5s;
+  }
 `
