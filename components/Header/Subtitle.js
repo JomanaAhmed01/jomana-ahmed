c85c670c3338cb1fa6c1516d6b7baa36b0d6b9b9
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-15 23:41:27 +0200

    Adding the responsive design to the Subtitle

--- components/Header/Subtitle.js
+++ components/Header/Subtitle.js
@@ -16,13 +16,21 @@ export const Wrapper = styled.div`
   margin-left: auto;
   margin-right: auto;
   width: 50%;
+
+  @media screen and (max-width: 470px) {
+    width: 80%;
+  }
 `
 
 export const SubTitle = styled.p`
-  font: 18px/1.9em "librebaskerville-regular", serif;
+  font: 25px/1.9em "librebaskerville-regular", serif;
   color: #a8a8a8;
   text-shadow: 0px 1px 2px rgb(0 0 0 / 50%);
   text-align: center;
+
+  @media screen and (max-width: 470px) {
+    font: 20px/1.9em "librebaskerville-regular", serif;
+  }
 `
 
 export const Span = styled.span`
