e4c93ac7d4f601e6d23c33b107efa80376d94333
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-15 23:35:02 +0200

    Creating the Responsive design for the title

--- components/Header/Title.js
+++ components/Header/Title.js
@@ -11,6 +11,8 @@ export default function Navbar() {
 export const Wrapper = styled.div`
   text-align: center;
   margin-top: 100px;
+  padding-left: 20px;
+  padding-right: 20px;
 `
 
 export const Title = styled.p`
@@ -20,4 +22,14 @@ export const Title = styled.p`
   letter-spacing: -2px;
   margin: 0 auto 18px auto;
   text-shadow: 0px 1px 3px rgb(0 0 0 / 80%);
+
+  @media screen and (max-width: 600px) {
+    font: 80px/1.1em "opensans-bold", sans-serif;
+    font-weight: bold;
+  }
+
+  @media screen and (max-width: 470px) {
+    font: 60px/1.1em "opensans-bold", sans-serif;
+    font-weight: bold;
+  }
 `
