dfbe16cbdca024373d2498e0884876ee60ee9087
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-15 22:00:22 +0200

    Linking the Social Links to the Social profiles

--- components/Header/SocialLinks.js
+++ components/Header/SocialLinks.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import Link from "next/link"
 import { Facebook } from "@styled-icons/boxicons-logos/Facebook"
 import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin"
 import { Instagram } from "@styled-icons/icomoon/Instagram"
@@ -8,9 +9,13 @@ export default function SocialLinks() {
   return (
     <Wrapper>
       <FacebookIcon />
-      <LinkedInIcon />
+      <Link href="https://www.linkedin.com/in/jomana-ahmed/">
+        <LinkedInIcon />
+      </Link>
       <InstagramIcon />
-      <GithubIcon />
+      <Link href="https://github.com/JomanaAhmed01">
+        <GithubIcon />
+      </Link>
     </Wrapper>
   )
 }
