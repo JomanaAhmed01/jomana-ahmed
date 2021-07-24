8469d5593b75f2c060eda16c70bff7c241423aa0
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-24 19:12:43 +0200

    Creating FooterWrapper

--- compounds/FooterCompound.js
+++ compounds/FooterCompound.js
@@ -3,9 +3,13 @@ import styled from "styled-components"
 export default function FooterCompound() {
   return (
     <Wrapper>
-      
+      <FooterWrapper></FooterWrapper>
     </Wrapper>
   )
 }
 
 export const Wrapper = styled.div``
+
+export const FooterWrapper = styled.div`
+  background-color: #303030;
+`
