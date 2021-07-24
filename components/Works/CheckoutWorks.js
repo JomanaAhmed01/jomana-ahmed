a667c607ab7fd63c05989b11ba1f14042b744293
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-24 17:10:26 +0200

    Fix CheckoutWorks

--- components/Works/CheckoutWorks.js
+++ components/Works/CheckoutWorks.js
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-export default function AboutMe() {
+export default function CheckoutWorks() {
   return (
     <Wrapper>
       <Title>CHECK OUT SOME OF MY WORKS.</Title>
