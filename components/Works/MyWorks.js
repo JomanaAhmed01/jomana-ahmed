bc9757f8a57ce7f28d6e19844fb224d92982f954
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-24 17:10:07 +0200

    Creating MyWorks file

---
+++ components/Works/MyWorks.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export default function MyWorks() {
+  return (
+    <Wrapper>
+        <Image src="/images/netflix-logo.jpg" />
+    </Wrapper>
+  )
+}
+
+export const Wrapper = styled.div``
+
+export const Image = styled.img``
--- compounds/WorksCompound.js
+++ compounds/WorksCompound.js
@@ -1,10 +1,12 @@
 import styled from "styled-components"
 import CheckoutWorks from '../components/Works/CheckoutWorks'
+import MyWorks from "../components/Works/MyWorks"
 
 export default function WorksCompound() {
   return (
     <Wrapper>
       <CheckoutWorks />
+      <MyWorks />
     </Wrapper>
   )
 }
