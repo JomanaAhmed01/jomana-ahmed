723cedb895d873907d513b60b812eb696cbc76ec
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-26 01:45:30 +0200

    Adding new features to the Images in the Works section

--- components/Works/MyWorks.js
+++ components/Works/MyWorks.js
@@ -1,11 +1,53 @@
 import styled from "styled-components"
+import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin"
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Earth } from "@styled-icons/ionicons-sharp/Earth"
 
 export default function MyWorks() {
   return (
     <Wrapper>
-      <ImageNetflix title="Netflix Project" src="/images/netflix.png" />
-      <ImageCrypto title="Crypto Platform Project" src="/images/crypto.png" />
-      <ImageBillie title="Billie Eilish Project" src="/images/billie.png" />
+      <ImageWrapper>
+        <ImageNetflix title="Netflix Project" src="/images/netflix.png" />
+        <IconsWrapper>
+          <a target="_blank" href="#">
+            <EarthIcon />
+          </a>
+          <a
+            target="_blank"
+            href="https://github.com/JomanaAhmed01/NextJS-netflix-clone"
+          >
+            <GithubIcon />
+          </a>
+        </IconsWrapper>
+      </ImageWrapper>
+      <ImageWrapper>
+        <ImageCrypto title="Crypto Platform Project" src="/images/crypto.png" />
+        <IconsWrapper>
+          <a target="_blank" href="#">
+            <EarthIcon />
+          </a>
+          <a
+            target="_blank"
+            href="https://github.com/JomanaAhmed01/crypto-platform-next"
+          >
+            <GithubIcon />
+          </a>
+        </IconsWrapper>
+      </ImageWrapper>
+      <ImageWrapper>
+        <ImageBillie title="Billie Eilish Project" src="/images/billie.png" />
+        <IconsWrapper>
+          <a target="_blank" href="#">
+            <EarthIcon />
+          </a>
+          <a
+            target="_blank"
+            href="https://github.com/JomanaAhmed01/Billie-Eilish-Website"
+          >
+            <GithubIcon />
+          </a>
+        </IconsWrapper>
+      </ImageWrapper>
     </Wrapper>
   )
 }
@@ -18,7 +60,7 @@ export const Wrapper = styled.div`
   margin-right: auto;
   margin-bottom: 40px;
 
-  @media screen and (max-width: 850px) {
+  @media screen and (max-width: 890px) {
     width: 46%;
     flex-direction: column;
     margin-left: auto;
@@ -26,41 +68,121 @@ export const Wrapper = styled.div`
   }
 `
 
+export const ImageWrapper = styled.div``
+
 export const ImageNetflix = styled.img`
   width: 215px;
-  height: 132px;
   cursor: pointer;
 
-  @media screen and (max-width: 850px) {
+  :hover {
+    width: 300px;
+    transition: 0.5s;
+  }
+
+  :not(:hover) {
+    transition: 0.5s;
+  }
+
+  @media screen and (max-width: 890px) {
     width: 100%;
     height: 50%;
-    margin-bottom: 40px;
+
+    :hover {
+      width: 100%;
+      transform: scale(1.2);
+      transition: 0.5s;
+      cursor: pointer;
+      margin-bottom: 20px;
+    }
   }
 `
 
 export const ImageCrypto = styled.img`
   width: 215px;
-  height: 132px;
   margin-left: 10px;
   margin-right: 10px;
   cursor: pointer;
 
-  @media screen and (max-width: 850px) {
+  :hover {
+    width: 300px;
+    transition: 0.5s;
+  }
+
+  :not(:hover) {
+    transition: 0.5s;
+  }
+
+  @media screen and (max-width: 890px) {
     width: 100%;
     height: 50%;
-    margin-bottom: 40px;
     margin-left: 0px;
     margin-right: 0px;
+
+    :hover {
+      width: 100%;
+      transform: scale(1.2);
+      transition: 0.5s;
+      cursor: pointer;
+      margin-bottom: 20px;
+    }
   }
 `
 
 export const ImageBillie = styled.img`
   width: 215px;
-  height: 132px;
   cursor: pointer;
 
-  @media screen and (max-width: 850px) {
+  :hover {
+    width: 300px;
+    transition: 0.5s;
+  }
+
+  :not(:hover) {
+    transition: 0.5s;
+  }
+
+  @media screen and (max-width: 890px) {
     width: 100%;
     height: 50%;
+
+    :hover {
+      width: 100%;
+      transform: scale(1.2);
+      transition: 0.5s;
+      cursor: pointer;
+      margin-bottom: 20px;
+    }
+  }
+`
+
+export const IconsWrapper = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: center;
+`
+
+export const GithubIcon = styled(Github)`
+  color: grey;
+  width: 35px;
+  height: 35px;
+  margin-left: 4px;
+  cursor: pointer;
+
+  :hover {
+    color: #66b3ff;
+    transition: 0.5s;
+  }
+`
+
+export const EarthIcon = styled(Earth)`
+  color: grey;
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+  cursor: pointer;
+
+  :hover {
+    color: #66b3ff;
+    transition: 0.5s;
   }
 `
