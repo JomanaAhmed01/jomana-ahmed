c74f4537c71954b399423a0ffbe8bc22a371127d
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-26 22:07:53 +0200

    Updating the Link & Fixing the Margin

--- components/About/ContactDetails.js
+++ components/About/ContactDetails.js
@@ -1,5 +1,4 @@
 import styled from "styled-components"
-import Link from "next/link"
 
 export default function AboutMe() {
   return (
@@ -8,21 +7,48 @@ export default function AboutMe() {
       <Subtitle>Jomana Ahmed</Subtitle>
       <Subtitle>Cairo, Egypt</Subtitle>
       <Subtitle>ojy.ahmed@outlook.com</Subtitle>
-      <a href="https://www.linkedin.com/in/jomana-ahmed/" target='_blank'><Subtitle>https://www.linkedin.com/in/jomana-ahmed/</Subtitle></a>
+      <a href="https://www.linkedin.com/in/jomana-ahmed/" target="_blank">
+        <LinkSubtitle>https://www.linkedin.com/in/jomana-ahmed/</LinkSubtitle>
+      </a>
     </Wrapper>
   )
 }
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  @media screen and (max-width: 890px) {
+    margin-left: -70px;
+  }
+`
 
 export const Title = styled.p`
   font: 22px/30px "opensans-bold", sans-serif;
   color: #fff;
   margin-bottom: 12px;
   font-weight: bold;
+
+  @media screen and (max-width: 768px) {
+    margin-left: 70px;
+  }
 `
 
 export const Subtitle = styled.p`
   line-height: 15px;
   color: #7a7a7a;
+
+  @media screen and (max-width: 768px) {
+    margin-left: 70px;
+  }
+`
+
+export const LinkSubtitle = styled.p`
+  line-height: 15px;
+  color: #1a53ff;
+
+  :hover {
+    text-decoration: underline;
+  }
+
+  @media screen and (max-width: 768px) {
+    margin-left: 70px;
+  }
 `
